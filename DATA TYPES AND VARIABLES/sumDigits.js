0bf89ae23489bd0844bb450dbd17d4aeeafc32dd
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2020-09-26 16:43:29 +0300

    Update sumDigits.js

--- DATA TYPES AND VARIABLES/sumDigits.js
+++ DATA TYPES AND VARIABLES/sumDigits.js
@@ -1,1 +1,9 @@
-
+function solve(input) { 
+   let str = input.toString();
+   let result = 0;
+   for(let i = 0; i <= str.length -1; i++) { 
+       let currentNumber = Number(str[i]);
+       result += currentNumber
+   }
+   console.log(result);
+}
