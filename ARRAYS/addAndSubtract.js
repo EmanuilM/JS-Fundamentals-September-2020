fdafa552aaf351544eb75b8baf40551fe92369b8
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2020-09-27 16:29:25 +0300

    Update addAndSubtract.js

--- ARRAYS/addAndSubtract.js
+++ ARRAYS/addAndSubtract.js
@@ -1,1 +1,22 @@
+function solve(arr = []) {
+    let modifyArray = arr.slice();
+    let originalArraySum = 0;
+    let modifyArrSum = 0;
 
+   for(let index1 = 0; index1 <= modifyArray.length - 1; index1++) { 
+
+    if(modifyArray[index1] % 2 == 0) { 
+        modifyArray[index1] += index1;
+    }else { 
+        modifyArray[index1] -= index1
+    }
+
+    originalArraySum += arr[index1];
+    modifyArrSum += modifyArray[index1];
+
+   }
+
+   console.log(modifyArray);
+   console.log(originalArraySum);
+   console.log(modifyArrSum);
+}
