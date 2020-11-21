b0cd377212bb7f3a4b78b9e1cae63e0218261931
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2020-11-21 22:00:45 +0200

    Create valueOfString.js

---
+++ Text Processing/valueOfString.js
@@ -0,0 +1,33 @@
+function solve(arr = []) {
+    arr = arr.filter((x) => x !== '');
+    let text = arr.shift();
+    let command = arr.shift();
+    let sum = 0;
+    
+    if(command === 'LOWERCASE') { 
+        text = text.split('');
+    
+        text.forEach(element => {
+            if(element === element.toLowerCase()) { 
+
+                if(element.charCodeAt(0) >= 65 && element.charCodeAt(0) <= 90 || element.charCodeAt(0) >= 97 && element.charCodeAt(0) <= 122) { 
+                    sum += element.charCodeAt(0);
+
+                }
+            }
+        });
+    }else if(command === 'UPPERCASE') { 
+        text = text.split('');
+        text.forEach(element => {
+            if(element === element.toUpperCase()) { 
+                if(element.charCodeAt(0) >= 65 && element.charCodeAt(0) <= 90 || element.charCodeAt(0) >= 97 && element.charCodeAt(0) <= 122) { 
+                    sum += element.charCodeAt(0);
+
+                }
+            }
+        });
+    }
+
+    console.log(`The total sum is: ${sum}`);
+
+}
