4445d11f18008b996421021cc0dfdab49aace061
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2020-11-22 18:06:34 +0200

    Create serializeString.js

---
+++ Text Processing/serializeString.js
@@ -0,0 +1,31 @@
+function solve(input) {
+    let text = input.shift().split('');
+   
+   let result = {};
+    
+   for (const index in text) {
+      
+        let char = text[index];
+        if(!result.hasOwnProperty(char)) { 
+
+            if(Number(index) !== - 1) { 
+                result[char] = [Number(index)];
+
+            }
+        }else { 
+
+            if(Number(index) !== -1) { 
+                result[char].push(Number(index));
+            }
+        }
+
+   }
+   
+   let entries = Object.entries(result);
+   
+
+   entries.forEach(element => {
+       console.log(`${element[0]}:${element[1].join('/')}`);
+   });
+   
+}
